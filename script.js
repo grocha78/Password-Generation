12d8d56927525fbeb753c5f229da9e969f23f4ca
Guadalupe Rocha <guadaluperocha@GuadalupesMBP2.attlocal.net>
2022-06-15 16:07:06 -0500

    working with js

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ window.alert("Welcome to Password Generator!");
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = lowerCase.toUpperCase();
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var specialCharacters = ["!","@","#","$","%","^","&","*"]
-console.log(upperCase)
 
 // Write password to the #password input
 function writePassword() {
@@ -18,7 +17,7 @@ function writePassword() {
 
 }
 
-function generatePassword(){
+function dataPreferences(){
 // Prompt user to choose password details
 var promptPassword = prompt("How many characters do you want in your password?");  
 if (promptPassword <8) {
@@ -29,10 +28,39 @@ if (promptPassword >128) {
   alert("Password needs to be between 8 and 128 characters!");
   return;
 }
+// Prompt user to confirm if they want to include lowercase characters
 var charactersLower = confirm("Do you want to include lowercase letters?");
+// Prompt user to confirm if they want to include upper case characters
 var charactersUpper = confirm("Do you want to include uppercase letters?");
+// Prompt user to confirm if they want to include number characters 
 var charactersNum = confirm("Do you want to include numbers?");
+// Prompt user to confirm if they want to include special characters
 var charactersSpecial = confirm("Do you want to include special characters?");
+
+var characterPref = {
+  passLength: promptPassword,
+  lowerCase: charactersLower,
+  upperCase: charactersUpper,
+  num: charactersNum,
+  specialChar: charactersSpecial
 }
+return characterPref;
+}
+
+function generatePassword() {
+  var userPref = dataPreferences();
+  console.log(userPref, "This returns user's password preferences");
+  if (characterPref.specialChar) {
+  var randoSpec = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+  }
+  console.log(randoSpec);
+
+
+
+
+
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
